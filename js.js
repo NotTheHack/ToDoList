e62a7f18886c07b1a757bb8bfbd9ab7b47a449d0
Caio Roberto Hack <106698188+NotTheHack@users.noreply.github.com>
2022-10-06 08:10:15 -0300

    Add files via upload

--- js.js
+++ js.js
@@ -3,16 +3,28 @@ function registerTask(){
     let newListValue = document.createElement("li")
     newListValue.innerText = task
     let removebutton = document.createElement("button")
+    let completebutton = document.createElement("button")
+    completebutton.type = "button"
+    completebutton.innerText = "Feito"
+    completebutton.setAttribute("onclick","completeTask(this)")
     removebutton.type = "button"
-    removebutton.innerText = "Feito"
-    removebutton.setAttribute("onclick","completeTask(this)")
+    removebutton.innerText = "X"
+    removebutton.setAttribute("onclick","removeTask(this)")
 
     newListValue.appendChild(removebutton)
+    newListValue.appendChild(completebutton)
 
     document.getElementById("tasklist").appendChild(newListValue)
 }
 
-function completeTask(button){
+function removeTask(button){
     liToRemove = button.parentNode
     document.getElementById("tasklist").removeChild(liToRemove)
+
+}
+
+function completeTask(button){
+    liToRemove = button.parentNode
+    document.getElementById("tasklist").appendChild(liToRemove).style.textDecoration = 'line-through';
+
 }
